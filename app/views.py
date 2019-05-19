b308e6bd0e6082b90e9b90d8d8772028c083c13c
Doktatech <Doktatech2@gmail.com>
2019-05-19 23:56:57 +0300

    define a function for rendering top articles

--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
 from flask import render_template
 from app import app
+from .request import get_news
 @app.route('/')
 def index():
     """
     Function that returns the index page and its data
     """
+    news_sources = get_news('name')
+    print (news_sources)
     title ='Home-Everywhere,Everytime News Room'
-    return render_template ('index.html',title = title)
+    return render_template ('index.html',title = title, news_source= news_sources)
 @app.route('/newssites/<news_id>')
 def newssites(news_id):
     """
