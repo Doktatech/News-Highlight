0a857ec7023360f3b35183dac734b8b83781f2e9
Doktatech <Doktatech2@gmail.com>
2019-05-19 18:27:12 +0300

    Add the base url for our API source

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     """
     A parent configuration class
     """
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/sources/{}?apiKey{}'
 class ProdConfig(Config):
     """
     A child production  configuration class that inherits from the parent Config class above
