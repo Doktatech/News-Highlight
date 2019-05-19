f62c696c03027109e44f73f48c66b16588244e70
Doktatech <Doktatech2@gmail.com>
2019-05-19 18:47:46 +0300

    Create a test instance for our news object

--- app/news_test.py
+++ app/news_test.py
@@ -5,4 +5,12 @@ class NewsTest(unittest.TestCase):
     """
     Test Class to test the behaviour of our News class
     """
-    
+    def setUp(self):
+        """
+        A setUp method to run before every test
+        """
+        self.new_news =News(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+if __name__=='__main__'
+    unittest.main()
