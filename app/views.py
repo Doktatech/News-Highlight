7424c7626edabdbb237b1b3137739d6c5838b4b7
Doktatech <Doktatech2@gmail.com>
2019-05-19 17:49:26 +0300

    Define a funcrion that renders different news sites

--- app/views.py
+++ app/views.py
@@ -7,3 +7,9 @@ def index():
     """
     Greetings= "Goodmorning Albazeera"
     return render_template ('index.html', Greeting=Greetings)
+@app.route('/newssites/<news_id>')
+def newssites(news_id):
+    """
+    Function that returns a news site and its details
+    """
+    return render_template ('newssite.html',id = news_id)
