54571a3db71dc7e973df846fe877f6551be36727
Doktatech <Doktatech2@gmail.com>
2019-05-19 18:26:48 +0300

    import the app instance configurations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
-app =Flask (__name__)
+app =Flask (__name__,instance_relative_config=True)
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config')
 from app import views
