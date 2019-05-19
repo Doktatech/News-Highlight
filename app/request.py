47aff5bc1191cd95c46c9b0de972ac53858d1396
Doktatech <Doktatech2@gmail.com>
2019-05-19 19:41:13 +0300

    Create a function for processing results

--- app/request.py
+++ app/request.py
@@ -15,3 +15,21 @@ def get_news(category):
         news_results =None
         if get_news_response =get_news_response['results']
         return news_results
+def process_results (news_list)
+    """
+    A function to process retrieved news result and display them on a list
+    """
+     news_results =[]
+     for news_item in news_list:
+        id=news_item.get('id')
+        title= news_item.get('title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+            news_object = News(id,title,overview,poster,vote_average,vote_count)
+            news_results.append(news_object)
+
+    return news_results
